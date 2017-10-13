058a3328da352d89a0ebcd3830f65870419e42a0
Lollipopsaurus <Den.Kilgore@gmail.com>
2017-10-13 10:59:37 -0400

    ingest setup script works

--- spot-ingest/ingest_setup.sh
+++ spot-ingest/ingest_setup.sh
@@ -23,7 +23,7 @@ source /etc/spot/spot.conf
 mkdir ${LUSER}/apache-spot-1.0-incubating/spot-ingest/src
 cd ${LUSER}/apache-spot-1.0-incubating/spot-ingest/src
 
-wget  --no-check-certificate https://bootstrap.pypa.io/get-pip.py 
+wget  --no-check-certificate https://bootstrap.pypa.io/get-pip.py
 sudo -H python get-pip.py
 
 sudo -H pip install kafka-python
@@ -32,19 +32,22 @@ sudo -H pip install watchdog
 
 sudo yum -y groupinstall "Development Tools"
 
-git clone https://github.com/Open-Network-Insight/spot-nfdump.git 
+git clone https://github.com/Open-Network-Insight/spot-nfdump.git
 cd spot-nfdump
-./install_nfdump.sh 
+./install_nfdump.sh
 cd ..
 
 sudo yum -y install gtk2-devel gtk+-devel bison qt-devel qt5-qtbase-devel sudo yum -y groupinstall "Development Tools"
 sudo yum -y install libpcap-devel
 #compile Wireshark
-wget https://1.na.dl.wireshark.org/src/wireshark-2.2.3.tar.bz2 tar xvf wireshark-2.0.1.tar.bz2
-cd wireshark-2.0.1
-./configure --with-gtk2 --disable-wireshark
-make 
-sudo make install
-cd ..
+#wget https://1.na.dl.wireshark.org/src/wireshark-2.2.9.tar.bz2 
+#tar xvf wireshark-2.2.9.tar.bz2
+#cd wireshark-2.2.9
+#./configure --with-gtk2 --disable-wireshark
+#make
+#sudo make install
+#cd ..
 
-wget https://repo1.maven.org/maven2/org/apache/spark/spark-streaming-kafka- 0-8-assembly_2.11/2.0.0/spark-streaming-kafka-0-8-assembly_2.11-2.0.0.jar 
+wget https://repo1.maven.org/maven2/org/apache/spark/spark-streaming-kafka-0-8-assembly_2.11/2.0.0/spark-streaming-kafka-0-8-assembly_2.11-2.0.0.jar
+mv spark-streaming-kafka-0-8-assembly_2.11-2.0.0.jar /opt/spot/spot-ingest/common
+
